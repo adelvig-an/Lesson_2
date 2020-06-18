aa0ef6e50c5aaf59d06c9c38e9003b707a53d16e
adelvig-an <63202963+adelvig-an@users.noreply.github.com>
2020-06-18 20:22:02 +0700

    Добавьте файлы проекта.

---
+++ Lesson_2/Model/MyArray.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lesson_2.Model
+{
+    public class MyArray
+    {
+        public int[] Arr { get; set; }
+
+        
+    }
+}
---
+++ Lesson_2/VM/FirstPageVM.cs
@@ -0,0 +1,49 @@
+﻿using Lesson_2.Model;
+using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Text;
+using System.Windows.Input;
+
+namespace Lesson_2.VM
+{
+    public class FirstPageVM : PageVM
+    {
+        public MyArray MyArray { get; }
+
+        public FirstPageVM()
+        {
+            MyArray = new MyArray
+            {
+                Arr = NewArray
+            };
+            Result = new RelayCommand(_ => ResultAction(MyArray));
+        }
+
+        private int[] newArray;
+        public int[] NewArray
+        {
+            get => newArray;
+            set => SetProperty(ref newArray, value);
+        }
+
+        private readonly int sum;
+        public int Sum
+        {
+            get => sum;
+            set
+            {
+                int sum = 0;
+                for (int i = 0; i < NewArray.Length; i++)
+                    sum += NewArray[i];
+            }
+        }
+
+        public ICommand Result { get; }
+        public void ResultAction(MyArray myArray)
+        {
+            myArray.Arr = NewArray;
+            OnPropertyChanged(nameof(MyArray));
+        }
+    }
+}
